1dd2e90c4b1d662211e3828f8cc87bd92b590474
Nathan Fennel <nathanfennel@gmail.com>
2026-08-03 17:26:30 +0200

    Run the browser launch UI test once

--- Straight Up BrowserUITests/Straight_Up_BrowserUITestsLaunchTests.swift
+++ Straight Up BrowserUITests/Straight_Up_BrowserUITestsLaunchTests.swift
@@ -9,10 +9,6 @@ import XCTest
 
 final class Straight_Up_BrowserUITestsLaunchTests: XCTestCase {
 
-    override class var runsForEachTargetApplicationUIConfiguration: Bool {
-        true
-    }
-
     override func setUpWithError() throws {
         continueAfterFailure = false
     }
